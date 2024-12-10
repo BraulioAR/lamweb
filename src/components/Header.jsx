799b467e3be6ed8004b4086ba1b39165ec7c0358
BraulioAR <braulior41@gmail.com>
2024-12-10 18:28:00 -0400

    fixed header logo sizing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,17 +11,17 @@ function Header() {
           <div className='hidden lg:block hover:scale-105 pt-2'>
             <Link href="/">
               <div>
-                <img src="/header-logo.webp" alt="LAM logo" className=' h-1/2 w-1/2 object-cover' />
+                <img src="/header-logo.jpg" alt="LAM logo" className='h-full w-full object-cover' />
               </div>
             </Link>
           </div>
 
           <div className='flex items-center justify-center lg:items-end lg:justify-end gap-x-2 lg:gap-x-4 '>
-            <Link href="/" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition:fill 0.3s'>Inicio</Link>
-            <Link href="/contactus" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition:fill 0.3s'>Contacto</Link>
-            <Link href="/acercaDe" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition:fill 0.3s'>Acerca de</Link>
-            <Link href="/#servicios" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition:fill 0.3s'>Servicios</Link>
-            <Link href="/portafolio" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition:fill 0.3s'>Portafolio</Link>
+            <Link href="/" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition-transform duration-300'>Inicio</Link>
+            <Link href="/contactus" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition-transform duration-300'>Contacto</Link>
+            <Link href="/acercaDe" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition-transform duration-300'>Acerca de</Link>
+            <Link href="/#servicios" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition-transform'>Servicios</Link>
+            <Link href="/portafolio" className='text-gray-600 lg:px-3 py-5 cursor-pointer text-sm lg:text-lg hover:scale-105 hover:text-black transition-transform'>Portafolio</Link>
           </div>
         </div>
       </div>
