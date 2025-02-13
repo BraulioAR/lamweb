e9d73402fe90595c16e70e3c2b2685cb44edfd21
BraulioAR <braulior41@gmail.com>
2025-02-13 15:03:00 -0400

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.lighstandmedia.com.do',
+  siteUrl: process.env.SITE_URL || 'https://www.lightsandmedia.com.do',
   generateRobotsTxt: true, // Genera robots.txt además del sitemap
   sitemapSize: 5000, // Opcional: divide el sitemap si excede este número
   robotsTxtOptions: {
